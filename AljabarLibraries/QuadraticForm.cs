73a2393a0f4f40fe6dcce784c6f9642b4c6a3ff1
Leirannt <93717185+Leirannt@users.noreply.github.com>
2023-05-03 01:10:02 +0700

    Add project files.

---
+++ AljabarLibraries/QuadraticForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AljabarLibraries
+{
+    public class quadraticForm
+    {
+        public static double[] akarKuadrat(double[] kuadrat)
+        {
+            double a = kuadrat[0];
+            double b = kuadrat[1];
+            double c = kuadrat[2];
+            double[] hasil = new double[2];
+
+            hasil[0] = (-b + Math.Sqrt((b * b) - (4 * a * c))) / (2 * a);
+            hasil[1] = (-b - Math.Sqrt((b * b) - (4 * a * c))) / (2 * a);
+            return hasil;
+        }
+        public static double[] kuadrat(double[] x)
+        {
+            double a = x[0];
+            double b = x[1];
+            double[] hasil = new double[3];
+
+            hasil[0] = Math.Pow(a, 2);
+            hasil[1] = 2 * a * b;
+            hasil[2] = Math.Pow(b, 2);
+            return hasil;
+        }
+
+    }
+}
---
+++ mainProgram/Program.cs
@@ -0,0 +1,13 @@
+﻿using AljabarLibraries;
+
+double[] APK = { 1, -3, -10 };
+double[] HS = { 2, -3 };
+
+double[] kecap = quadraticForm.akarKuadrat(APK);
+double[] pelangi = quadraticForm.kuadrat(HS);
+
+Console.WriteLine("{" + kecap[0] + ", " + kecap[1] + "}");
+Console.WriteLine("{" + pelangi[0] + ", "+ pelangi[1] + ", " + pelangi[2] + "}");
+
+
+
